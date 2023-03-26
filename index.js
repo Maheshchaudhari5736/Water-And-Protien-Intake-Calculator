8fad022c104aee1372967024757313379cccfaf8
Mahesh Chaudhari <126462898+Maheshchaudhari5736@users.noreply.github.com>
2023-03-26 12:53:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const getResult=()=>{
+    let enter= document.getElementById('weightInput').value;
+    let water = (enter/28).toFixed(2);
+    let show=document.getElementById('showResult');
+
+    if(enter==""){
+      show.innerHTML="Please enter data";    
+    }else{
+      show.innerHTML=`You should take ${water}L of water and ${enter}g of protein per day`;
+    }
+  }
